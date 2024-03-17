fb0b8bf2056d4790cd416a0135e1e54ca09da7c3
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2024-03-17 17:48:27 +0530

    WIP: Auth implementation using Devise and Doorkeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validate :password_complexity
+  validates :email, presence: true
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be atleast characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  # the authenticate method from devise documentation
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
 end
